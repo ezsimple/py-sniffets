3cfbc972fb81e61636896afb676a1cda1bfb084e
mhlee <mkeasy@gmail.com>
2023-05-30 11:54:12 +0900

    .

--- crontab.sh
+++ crontab.sh
@@ -2,4 +2,6 @@
 HOME=/home/ubuntu
 PATH=.:$HOME:$HOME/.virtualenvs/머신러닝/bin:$HOME/.local/bin:.:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 WORK_DIR=/home/ubuntu/py-sniffets
-( cd $WORK_DIR; ./tcafe_session.py )
+
+( cd $WORK_DIR; ./tcafe_playwright.py )
+( cd $WORK_DIR; ./jk.py )

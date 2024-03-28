f74edd35e092731f2be5e2f3571bfaf5e7254204
mhlee <mkeasy@gmail.com>
2024-03-28 15:35:31 +0900

    .

--- tcafe_playwright.py
+++ tcafe_playwright.py
@@ -10,23 +10,25 @@ import re
 from bs4 import BeautifulSoup
 from LogUtil import LogUtil
 
-TOKEN = '5758487515:AAFfZ9fZsv7padX_6StJbn3T9zFOvW46jcc'
-CHAT_ID = '918743728'
+TOKEN = "5758487515:AAFfZ9fZsv7padX_6StJbn3T9zFOvW46jcc"
+CHAT_ID = "918743728"
 URL = "http://tcafe2a.com"
-LOGGING = LogUtil('TCAFE_ACCESS')
+LOGGING = LogUtil("TCAFE_ACCESS")
+
 
 def send_message(text):
 
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     params = {
-      "chat_id": CHAT_ID,
-      "text": text,
+        "chat_id": CHAT_ID,
+        "text": text,
     }
     resp = requests.get(url, params=params)
     LOGGING.debug(text)
 
     # Throw an exception if Telegram API fails
     resp.raise_for_status()
+
 
 def close_notify(page: Page):
     try:
@@ -35,40 +37,52 @@ def close_notify(page: Page):
     except TimeoutError:
         pass
 
+
 # async def report():
 #     today = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 #     msg = "{} 출석완료".format(today)
 #     bot = telegram.Bot(token = TOKEN)
 #     await bot.send_message(CHAT_ID, msg)
 
+
 def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
     page = context.new_page()
-    page.goto(URL)
 
-    close_notify(page)
+    try:
+        page.goto(URL)
 
-    page.locator("#ol_id").click()
-    page.locator("#ol_id").fill("mkeasy")
-    page.locator("#ol_pw").click()
-    page.locator("#ol_pw").fill("dodls9gka")
-    page.locator("#ol_pw").press("Enter")
-    page.get_by_role("link", name="출석확인", exact=True).click()
-    page.locator("#cnftjr img").click()
-    page.wait_for_timeout(1000)
+        close_notify(page)
 
-    # 출첵확인
-    html =page.content()
-    txt = re.compile(r'출석.*주세요|출석.*획득').findall(html)
-    msg = '출석 확인 필요합니다.' if len(txt) == 0 else ''.join(txt)
-    msg = BeautifulSoup(msg, "html.parser").text # html태그 제거(xml 구조 오류가 있어도 무시하고 파싱처리 됩니다. 단 처리속도가 다소 늦습니다.)
-    send_message(msg)
+        page.locator("#ol_id").click()
+        page.locator("#ol_id").fill("mkeasy")
+        page.locator("#ol_pw").click()
+        page.locator("#ol_pw").fill("dodls9gka")
+        page.locator("#ol_pw").press("Enter")
+        page.get_by_role("link", name="출석확인", exact=True).click()
+        page.locator("#cnftjr img").click()
+        page.wait_for_timeout(1000)
 
-    page.close()
-    # ---------------------
-    context.close()
-    browser.close()
+        # 출첵확인
+        html = page.content()
+        txt = re.compile(r"출석.*주세요|출석.*획득").findall(html)
+        msg = "출석 확인 필요합니다." if len(txt) == 0 else "".join(txt)
+        msg = BeautifulSoup(
+            msg, "html.parser"
+        ).text  # html태그 제거(xml 구조 오류가 있어도 무시하고 파싱처리 됩니다. 단 처리속도가 다소 늦습니다.)
+        send_message(msg)
+
+    except Exception as e:
+        send_message("#ERROR# tcafe2a 출책 실패")
+        pass
+
+    finally:
+        page.close()
+        # ---------------------
+        context.close()
+        browser.close()
+
 
 with sync_playwright() as playwright:
-  run(playwright)
+    run(playwright)

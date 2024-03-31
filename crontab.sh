8e17b3d7524a2f4f392f79d01b475cdcb2a43f2e
mhlee <mkeasy@gmail.com>
2024-03-31 13:21:38 +0900

    .

--- crontab.sh
+++ crontab.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# 10 9 * * * /home/ubuntu/py-sniffets/crontab.sh
+
 HOME=/home/ubuntu
 PATH=.:$HOME:$HOME/.virtualenvs/머신러닝/bin:$HOME/.local/bin:.:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 WORK_DIR=/home/ubuntu/py-sniffets

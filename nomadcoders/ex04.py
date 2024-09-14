7b4ad1cd5457114825b97ef52c3e0183875eee5f
mhlee <mkeasy@gmail.com>
2024-09-14 15:24:09 +0900

    거북이로 장미 그리기

--- nomadcoders/ex04.py
+++ nomadcoders/ex04.py
@@ -3,49 +3,13 @@
 
 import turtle
 
-# 설정
-t = turtle.Turtle()
-t.speed(0)  # 최대 속도
+# 거북이 모양으로 변경
+turtle.shape("turtle")
 
-# 장미 꽃잎 그리기 함수
-def draw_petal():
-    t.color("red")
-    t.begin_fill()
-    t.circle(50, 60)
-    t.left(120)
-    t.circle(50, 60)
-    t.left(120)
-    t.end_fill()
+# 삼각형 그리기
+for _ in range(3):
+    turtle.forward(100)  # 거북이가 100 만큼 앞으로 이동
+    turtle.left(120)     # 거북이가 왼쪽으로 120도 회전
 
-# 장미 그리기
-for _ in range(6):
-    draw_petal()
-    t.right(60)
-
-# 줄기 그리기
-t.color("green")
-t.right(90)
-t.forward(200)
-
-# 잎사귀 그리기
-t.right(45)
-t.color("green")
-t.begin_fill()
-t.circle(50, 90)
-t.left(90)
-t.circle(50, 90)
-t.end_fill()
-
-# 다른 잎사귀 그리기
-t.left(135)
-t.forward(100)
-t.left(45)
-t.begin_fill()
-t.circle(50, 90)
-t.left(90)
-t.circle(50, 90)
-t.end_fill()
-
-# 완료
+# 화면을 클릭하면 종료
 turtle.done()
-

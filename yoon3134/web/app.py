08c54b0493fb60e12b434167b34ef0e70270ce79
mhlee <mkeasy@gmail.com>
2024-09-20 21:29:40 +0900

    .

--- yoon3134/web/app.py
+++ yoon3134/web/app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 # 이미지 목록
 PORT = 5000
 PATH = 'static' # Flask에서는 정적 파일(이미지등)을 제공하기 위해선 static 디렉토리를 사용합니다.
-HOST = 'http://127.0.0.1:{PORT}'
+HOST = 'http://127.0.0.1'
 
 images = [
-    {'url': f'{HOST}/{PATH}/image1.jpg', 'title': '이미지 1'},
-    {'url': f'{HOST}/{PATH}/image2.jpg', 'title': '이미지 2'},
-    {'url': f'{HOST}/{PATH}/image3.jpg', 'title': '이미지 3'},
+    {'url': f'{HOST}:{PORT}/{PATH}/image1.jpg', 'title': '이미지 1'},
+    {'url': f'{HOST}:{PORT}/{PATH}/image2.jpg', 'title': '이미지 2'},
+    {'url': f'{HOST}:{PORT}/{PATH}/image3.jpg', 'title': '이미지 3'},
 ]
 
 @app.route('/')

adee2ca6d295734df886433ca6e94fdd8e1d8d26
mhlee <mkeasy@gmail.com>
2024-09-20 18:05:21 +0900

    맥의 일부 디렉토리명의 자소분리 현상 고쳐지지 않는 문제 보완

--- nfd2nfc.py
+++ nfd2nfc.py
@@ -1,59 +1,51 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# 맥에서 한글 파일명 자소분리 문제 해결
-# 이미 자소분리되어 있는 파일명을 NFC형식으로 변환해 주는 유틸리티
-# - 한글 자소 분리 문제
-# - Mac은 한글 문자의 정규화 방식으로 NFD(Normalization Form Decomposition)를 사용하는데, 
-#   Windows나 대부분의 Linux distribution에서는 NFC(Normalization Form Decomposition)를 사용하기 때문
-# 
-# https://devocean.sk.com/blog/techBoardDetail.do?ID=164376
-
 import argparse
 import os
 import unicodedata
 
 ####################################################################
-## 파일명을 NFC(완성형) 를 NFD(조합형) 로 변경
+## 파일명 및 폴더명을 NFC(완성형)로 변경
 ####################################################################
-def normalize_filename(file_path):
-    # 파일명에서 디렉터리 경로와 파일명을 분리
-    directory, filename = os.path.split(file_path)
+def normalize_path(path):
+    # NFC로 정규화한 경로 생성
+    normalized_path = unicodedata.normalize('NFC', path)
 
-    # NFC로 정규화한 파일명 생성
-    normalized_filename = unicodedata.normalize('NFC', filename)
-
-    # 디렉터리와 정규화된 파일명을 합쳐서 새로운 파일 경로 생성
-    normalized_file_path = os.path.join(directory, normalized_filename)
-
-    # 파일명 변경
-    os.rename(file_path, normalized_file_path)
-    print(f"[+] {file_path} -> {normalized_file_path}")
-
+    # 경로가 변경되었는지 확인
+    if path != normalized_path:
+        # 수정된 경로로 변경
+        os.rename(path, normalized_path)
+        print(f"[+] {path} -> {normalized_path}")
 
 ####################################################################
-## 변경 대상 파일명 찾아서 normalize_filename() 호출
+## 변경 대상 파일 및 폴더명 찾아서 normalize_path() 호출
 ####################################################################
 def normalize_filenames_in_directory(directory, recursive=False, target_extension=None):
     for dirpath, dirnames, filenames in os.walk(directory):
+        # 현재 디렉터리에서 폴더 이름 정규화
+        for dirname in dirnames:
+            dir_path = os.path.join(dirpath, dirname)
+            normalize_path(dir_path)
+
+        # 파일 이름 정규화
         for filename in filenames:
             file_path = os.path.join(dirpath, filename)
 
             # 특정 확장자에 해당하는 파일만 변경
             if target_extension is None or file_path.lower().endswith(target_extension.lower()):
-                normalize_filename(file_path)
+                normalize_path(file_path)
 
         # 만약 recursive 옵션이 활성화되었으면, 하위 디렉터리의 탐색을 계속
         if not recursive:
             break
-
 
 ####################################################################
 ## 인자 설정 및 변경 코드 호출
 ####################################################################
 def main():
     parser = argparse.ArgumentParser(
-        description='Normalize Korean filenames in a directory.',
+        description='Normalize Korean filenames and directory names in a directory.',
         usage='%(prog)s [directory] [-r|--recursive] [-e|--extension <extension>]'
     )
     parser.add_argument('directory', metavar='directory', type=str, nargs='?', default=os.getcwd(),
@@ -68,7 +60,7 @@ def main():
     recursive = args.recursive
     target_extension = args.extension
 
-    # 디렉터리 내의 특정 확장자를 가진 파일들의 이름을 NFC로 정규화
+    # 디렉터리 내의 특정 확장자를 가진 파일들과 폴더들의 이름을 NFC로 정규화
     normalize_filenames_in_directory(target_directory, recursive, target_extension)
 
 if __name__ == '__main__':

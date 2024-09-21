a923903f37f1eaa68cc75b428f2b1b4d476c7f54
mhlee <mkeasy@gmail.com>
2024-09-21 15:45:27 +0900

    .

--- yoon3134/web/app.py
+++ yoon3134/web/app.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # 이미지 목록
+HOST = '127.0.0.1'
 PORT = 5000
 PATH = 'static' # Flask에서는 정적 파일(이미지등)을 제공하기 위해선 static 디렉토리를 사용합니다.
-HOST = 'http://127.0.0.1'
 
 images = [
     {'url': f'{PATH}/image1.jpg', 'title': '이미지 1'},
@@ -18,4 +18,4 @@ def gallery():
     return render_template('gallery.html', images=images)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=PORT)
+    app.run(debug=True, host=HOST, port=PORT)

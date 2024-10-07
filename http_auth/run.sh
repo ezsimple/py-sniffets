55ec577e567dff62664aa005f85712ef23aba9b2
mhlee <mkeasy@gmail.com>
2024-10-08 00:08:56 +0900

    .

--- http_auth/run.sh
+++ http_auth/run.sh
@@ -33,7 +33,7 @@ fi
 # 4. nohup을 이용하여 FastAPI 애플리케이션 실행
 echo "FastAPI 애플리케이션을 nohup으로 실행합니다."
 # --reload 옵션은 python -m multiprocess를 실행시켜 서브프로세스를 따로 생성시킴
-nohup python -m uvicorn app:app --host $HOST --port $PORT --log-level debug > nohup.out 2>&1 &
+nohup python -m uvicorn app:app --host $HOST --port $PORT > nohup.out 2>&1 &
 
 echo "애플리케이션이 백그라운드에서 실행 중입니다."
 

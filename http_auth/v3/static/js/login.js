2b3a2731257cf68d23ae5ac2cb8775994d679744
mhlee <mkeasy@gmail.com>
2024-10-17 16:03:36 +0900

    .

--- http_auth/v3/static/js/login.js
+++ http_auth/v3/static/js/login.js
@@ -15,12 +15,18 @@ document.getElementById("loginForm").addEventListener("submit", async (event) =>
         }),
     });
 
-    if (response.ok) {
-        const data = await response.json();
-        console.log("로그인 성공:", data.access_token);
-        localStorage.setItem("token", data.access_token);
-        // window.location.href = "/v3/files"; // 로그인 성공 시 이동
-    } else {
-        alert("로그인 실패: 사용자 이름 또는 비밀번호가 잘못되었습니다.");
+    try {
+        if (response.ok) {
+            const data = await response.json();
+            console.log("로그인 성공:", data.access_token);
+            localStorage.setItem("token", data.access_token);
+            // window.location.href = "/v3/files"; // 로그인 성공 시 이동
+        } else {
+            // 400오류, HTTP
+            const errorData = await response.json();
+            alert(`로그인 실패: ${errorData.detail}`);
+        }
+    } catch (error) {
+        alert(error)
     }
 });

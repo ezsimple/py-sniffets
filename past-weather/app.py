b7e3a517c3c60c1759239f5407dad9e997b12bb1
mhlee <mkeasy@gmail.com>
2024-11-16 16:04:31 +0900

    .

--- past-weather/app.py
+++ past-weather/app.py
@@ -3,20 +3,22 @@ from fastapi import FastAPI, APIRouter
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import RedirectResponse, HTMLResponse
 from starlette.requests import Request
-from datetime import datetime
 from core.config import PREFIX, logging
-from core.model import CustomTemplateResponse
+from core.model import CustomTemplateResponse, RedirectGetResponse
+from sqlalchemy import extract, func
 from sqlalchemy.orm import sessionmaker
 from database.create_tables import engine, MinoWeatherMonthly, MinoWeatherDaily
-import pandas as pd
-import altair as alt
+from database.weather_visualization import WeatherVisualization
+from datetime import datetime
+import calendar
 
 app = FastAPI(title="Past Weather(송악읍)", version="0.0.1")
-app.mount("/past-weather/static", StaticFiles(directory="static"), name="static")
+app.mount(f"{PREFIX}/static", StaticFiles(directory="static"), name="static")
 router = APIRouter(prefix=PREFIX)
 logger = logging.getLogger(__name__)
 SessionLocal = sessionmaker(bind=engine)
 CITY = 'songak'
+MIN_YEAR = 2014
 
 @router.get("/", response_class=RedirectResponse)
 async def to_firstpage(request: Request):
@@ -25,59 +27,133 @@ async def to_firstpage(request: Request):
     return CustomTemplateResponse("index.html", {"request": request})
 
 @router.get("/{city:str}/{yyyy:str}", response_class=HTMLResponse)
-async def year_chart(request: Request, city: str, yyyy: str):
+async def monthly_chart(request: Request, city: str, yyyy: str):
+    '''
+    년-월별 차트 생성
+    '''
+    if not yyyy.isdigit():
+        yyyy = datetime.now().year  
+        return RedirectGetResponse(url=f"{PREFIX}/{city}/{yyyy}")
+
+    current_year = datetime.now().year
+    if MIN_YEAR > int(yyyy) or int(yyyy) > current_year:
+        yyyy = current_year
+        return RedirectGetResponse(url=f"{PREFIX}/{city}/{yyyy}")
+
     logger.info(f"city: {city}, yyyy: {yyyy}")
 
     session = SessionLocal()
+    data = {
+        '일자': [],
+        '온도(°C)': [],
+        '습도(%)': [],
+        '강수량(mm)': [],
+        '강수형태': []
+    }
+
     try:
         query = session.query(MinoWeatherMonthly)\
             .filter(MinoWeatherMonthly.measure_month.like(f"{yyyy}%"))\
             .order_by(MinoWeatherMonthly.measure_month.asc())
-        # logger.debug(str(query))
+        logger.debug(str(query))
         monthly_data = query.all()
-        session.close()
+
+        for entry in monthly_data:
+            data['일자'].append(entry.measure_month)
+            data['강수량(mm)'].append(int(entry.sum_precipitation))
+            data['강수형태'].append(int(entry.precipitation_type))
+            data['습도(%)'].append(int(entry.avg_humidity))
+            data['온도(°C)'].append(int(entry.avg_temperature))
+
+        # 클래스 인스턴스 생성
+        weather_viz = WeatherVisualization(data)
+        # 차트 생성
+        combined_chart = weather_viz.combined_chart().to_html()
+
     except Exception as e:
-        logger.error(e)
+        logger.error(f"Error occurred: {e}")
+        return HTMLResponse(content="데이터를 가져오는 중 오류가 발생했습니다.", status_code=500)
+
     finally:
         session.close()
 
-    data = {
-        'month': [],
-        'precipitation': [],
-        'humidity': [],
-        'temperature': []
-    }
-
-    for entry in monthly_data:
-        data['month'].append(entry.measure_month)
-        data['precipitation'].append(int(entry.sum_precipitation))
-        data['humidity'].append(int(entry.avg_humidity))
-        data['temperature'].append(int(entry.avg_temperature))
-    
-    df = pd.DataFrame(data)
-    # df['month'] = pd.to_datetime(df['month'], format='%Y-%m')
-
-     # Altair 차트 생성
-    daily_precipitation_chart = alt.Chart(df).mark_bar(color='green').encode(
-        x='일:T',
-        y='강수량:Q',
-        tooltip=['일:T', '강수량:Q']
-    ).properties(title='일별 강수량 변화')
-
-    daily_temperature_chart = alt.Chart(df).mark_line(color='orange').encode(
-        x='일:T',
-        y='평균 온도:Q',
-        tooltip=['일:T', '평균 온도:Q']
-    ).properties(title='일별 평균 온도 변화')
-
-    combined_chart = alt.vconcat(daily_precipitation_chart, daily_temperature_chart)
-
-    return CustomTemplateResponse("year.html", {"request": request, "chart": combined_chart.to_html()})
+    return CustomTemplateResponse("chart.html", {"request": request, "chart": combined_chart})
 
 @router.get("/{city:str}/{yyyy:str}/{mm:str}", response_class=HTMLResponse)
-async def month_chart(request: Request, city: str, yyyy: str, mm: str):
+async def daily_chart(request: Request, city: str, yyyy: str, mm: str):
+    '''
+    일별 강수량, 일별 평균 온도 그래프
+    '''
+    session = SessionLocal()
+    # 현재 년도 가져오기
+    current_year = datetime.now().year
+
+    # yyyy 검증 및 최대 월 가져오기
+    if not str(yyyy).isdigit() or (MIN_YEAR > int(yyyy) or int(yyyy) > current_year):
+        yyyy = current_year  # 유효하지 않으면 현재년도로 대체
+        mm = get_max_month_for_year(session, yyyy)  # 최대 월 가져오기
+        return get_redirect_url(city, yyyy, mm)
+
+    # mm 검증 및 최대 월 가져오기
+    if not str(mm).isdigit() or (1 > int(mm) or int(mm) > 12):
+        mm = get_max_month_for_year(session, yyyy)  # 최대 월 가져오기
+        return get_redirect_url(city, yyyy, mm)
+
     logger.info(f"city: {city}, yyyy: {yyyy}, mm: {mm}")
-    return CustomTemplateResponse("month.html", {"request": request})
+
+    data = {
+        '일자': [],
+        '온도(°C)': [],
+        '습도(%)': [],
+        '강수량(mm)': [],
+        '강수형태': []
+    }
+
+    try:
+        start_date = f"{yyyy}-{mm}-01"  # 시작일
+        last_day = calendar.monthrange(int(yyyy), int(mm))[1]  # 해당 월의 마지막 날짜
+        end_date = f"{yyyy}-{mm}-{last_day}"  # 종료일
+        query = session.query(MinoWeatherDaily)\
+            .filter(
+                MinoWeatherDaily.measure_day >= start_date,
+                MinoWeatherDaily.measure_day <= end_date
+            )\
+            .order_by(MinoWeatherDaily.measure_day.asc())
+        logger.debug(str(query))
+        daily_data = query.all()
+
+        for entry in daily_data:
+            data['일자'].append(entry.measure_day.strftime('%Y-%m-%d'))  # 날짜를 문자열로 변환
+            data['강수량(mm)'].append(int(entry.sum_precipitation))
+            data['강수형태'].append(int(entry.precipitation_type))
+            data['습도(%)'].append(int(entry.avg_humidity))
+            data['온도(°C)'].append(int(entry.avg_temperature))
+
+        # 클래스 인스턴스 생성
+        weather_viz = WeatherVisualization(data, 'daily')
+        # 차트 생성
+        combined_chart = weather_viz.combined_chart().to_html()
+
+    except Exception as e:
+        logger.error(f"Error occurred: {e}")
+        return HTMLResponse(content="데이터를 가져오는 중 오류가 발생했습니다.", status_code=500)
+
+    finally:
+        session.close()
+
+    return CustomTemplateResponse("chart.html", {"request": request, "chart": combined_chart})
+
+def get_max_month_for_year(session, year):
+    """주어진 년도의 최대 월을 반환하는 함수"""
+    return session.query(
+        func.max(extract('month', MinoWeatherDaily.measure_day)).label('max_month')
+    ).filter(
+        extract('year', MinoWeatherDaily.measure_day) == year
+    ).scalar()
+
+def get_redirect_url(city, yyyy, mm):
+    """리다이렉트 URL 생성 함수"""
+    return RedirectGetResponse(url=f"{PREFIX}/{city}/{yyyy}/{mm}")
 
 app.include_router(router)  # 라우터 등록
 

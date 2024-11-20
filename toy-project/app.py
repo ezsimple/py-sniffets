ad6fccc1d8035dc22fe6fd480d7faa1901e01afa
mhlee <mkeasy@gmail.com>
2024-11-20 21:45:35 +0900

    wsgi 추가

--- toy-project/app.py
+++ toy-project/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+    @app.route('/')
+    def home():
+        return render_template('index.html')
+
+    return app
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app = create_app()  # create_app 함수 호출
+    app.run(debug=True)  # 디버그 모드에서 실행
--- toy-project/wsgi.py
+++ toy-project/wsgi.py
@@ -1,5 +1,16 @@
+import os
 import sys
 sys.path.append('/home/ubuntu/.pyenv/versions/3.10.14/envs/머신러닝/lib/python3.10/site-packages')
 
+# Flask 애플리케이션을 생성하는 함수 임포트
 from app import create_app
+
+# 환경 변수 설정
+env = os.getenv('APPLICATION_MODE', 'PRODUCTION')
+
+# Flask 애플리케이션 인스턴스 생성
 application = create_app()
+
+if __name__ == "__main__":
+    # 개발 환경에서 실행할 때는 기본 웹서버로 실행
+    application.run()

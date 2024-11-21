6bd37795443019677cd62f035438e7b286ac0265
mhlee <mkeasy@gmail.com>
2024-11-21 12:31:06 +0900

    --touch-reload를 이용하여 static 파일변경시 바로 반영

--- toy-project/run.sh
+++ toy-project/run.sh
@@ -7,4 +7,5 @@ export APPLICATION_MODE=PRODUCTION
 PATH=.:$PATH:/home/ubuntu/.pyenv/shims
 
 [[ -f master.pid ]] && kill -9 `cat master.pid`
-nohup uwsgi --ini uwsgi-toy-project.ini &
+# --touch-reload를 이용하여 static 파일변경시 바로 반영
+nohup uwsgi --ini uwsgi-toy-project.ini --touch-reload $(pwd) &

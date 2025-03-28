381c868b5a1ada0bbf37284544e58a8e43ae9263
mhlee <mkeasy@gmail.com>
2025-03-28 23:38:13 +0900

    .

--- a1_monitoring/a1_monitoring.py
+++ a1_monitoring/a1_monitoring.py
@@ -2,6 +2,7 @@
 import aiohttp
 import asyncio
 import requests
+import subprocess
 
 # 주의 : 버츄얼 호스트수(len(URLs))가 10을 넘으면 nginx.limit_req burst=10으로 인해 503 발생함.
 HOST = "https://a1.mkeasy.kro.kr"
@@ -16,6 +17,14 @@ URIs = {
     "/auth/health": "keycloak", # keycloak
     "/holiday/health": "calendar api", # calendar api
 }
+async def check_nginx_status():
+    try:
+        # Nginx 상태 확인
+        result = subprocess.run(['systemctl', 'is-active', 'nginx'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return result.stdout.decode('utf-8').strip() == 'active'
+    except Exception as e:
+        print(f"Error checking Nginx status: {e}")
+        return False
 
 async def send_message(text):
     TOKEN = "5758487515:AAFfZ9fZsv7padX_6StJbn3T9zFOvW46jcc"
@@ -31,12 +40,24 @@ async def send_message(text):
     resp.raise_for_status()
 
 async def fetch_status(session, url):
-    async with session.get(url) as response:
-        if response.status != 200:
-            msg = f"{url} is down! Status code: {response.status}"
-            desc = URIs[url]
-            service_name = list(URIs.keys())[list(URIs.values()).index(desc)]
-            await send_message(f'{msg} ({service_name})')
+    try:
+        async with session.get(url) as response:
+            if response.status != 200:
+                msg = f"{url} is down! Status code: {response.status}"
+                desc = URIs[url]
+                service_name = list(URIs.keys())[list(URIs.values()).index(desc)]
+                await send_message(f'{msg} ({service_name})')
+    except aiohttp.ClientConnectorError:
+        msg = f"Cannot connect to {url}"
+        desc = URIs[url]
+        service_name = list(URIs.keys())[list(URIs.values()).index(desc)]
+        await send_message(f'{msg} ({service_name})')
+    except Exception as e:
+        msg = f"An error occurred while checking {url}: {e}"
+        desc = URIs[url]
+        service_name = list(URIs.keys())[list(URIs.values()).index(desc)]
+        await send_message(f'{msg} ({service_name})')
+
 
 async def check_urls(urls):
     async with aiohttp.ClientSession() as session:
@@ -44,8 +65,18 @@ async def check_urls(urls):
         await asyncio.gather(*tasks)
 
 async def main():
-    urls_to_check = [f"{HOST}{uri}" for uri in URIs]  # Get URLs from dictionary keys
-    await check_urls(urls_to_check)
+    try:
+        if not await check_nginx_status():
+            msg = 'Nginx service is not running!'
+            await send_message(f'{msg}')
+
+
+        urls_to_check = [f"{HOST}{uri}" for uri in URIs]
+        await check_urls(urls_to_check)
+
+    except Exception as e:
+        msg = '#오류# service is not running!'
+        await send_message(f'{msg}')
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
